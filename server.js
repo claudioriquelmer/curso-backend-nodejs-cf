6398c48c63ab950a1e855d2c891e71cf139275ea
Claudio <claudio.riquelme@consultores.wom.cl>
2024-06-04 18:26:51 -0400

    Formulario de Ingreso

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ const methodOverride = require('method-override');
 
 const app = express();
 const tasksRoutes = require('./routes/tasks_routes');
+const registrationsRoutes = require('./routes/registrations_routes');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.set('view engine','pug');
 
 app.use(tasksRoutes);
+app.use(registrationsRoutes);
 
 app.listen(4080);
