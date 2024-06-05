6fab9ac6dc2d9a57d7c2a4941a7b30305f5ff8a2
Claudio <claudio.riquelme@consultores.wom.cl>
2024-06-05 16:19:33 -0400

    Creacion de usuarios con pass hash

---
+++ models/user.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const { DataTypes } = require("sequelize");
+const bcrypt = require('bcrypt');
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    password_hash: DataTypes.STRING,
+    password: DataTypes.VIRTUAL
+  }, {});
+  User.associate = function(models) {
+
+  };
+  User.beforeCreate(function(user,options) {
+    return new Promise((res,rej) => {
+      if(user.password) {     
+        bcrypt.hash(user.password, 10, function(error,hash) {
+          user.password_hash = hash;
+          res();
+        })
+      };
+    });     
+  });
+  return User;
+}
